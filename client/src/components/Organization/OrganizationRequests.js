b0e43316033332c2139e18fdc98cc9aa5e612b07
Jeremy Grubard <jgrubard@gmail.com>
2018-05-30 19:48:30 -0400

    can see if user has been accepted or declined

--- client/src/components/Organization/OrganizationRequests.js
+++ client/src/components/Organization/OrganizationRequests.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { updateOrganizationRequestOnServer } from '../../store';
 
-const OrganizationRequests = ({ organization, ownUsers, acceptUser, declineUser, organizationRequests }) => {
-  return (
-    <div>
-      {
-        ownUsers.map(user => {
-          const ownRequest = organizationRequests.find(request => request.userId === user.id && request.organizationId === organization.id);
-          const { id, userId, organizationId } = ownRequest;
-          return (
-            <div key={user.id}>
-              {user.fullName}
-              <button onClick={() => acceptUser({ id, userId, organizationId, status: 'accepted' })}>Accept</button>
-              <button onClick={() => declineUser({ id, userId, organizationId, status: 'declined' })}>Decline</button>
-            </div>
-          )
-        })
-      }
-    </div>
-  );
+class OrganizationRequests extends Component {
+  constructor() {
+    super();
+    this.state = { requestStatus: '' }
+    this.accept = this.accept.bind(this);
+    this.decline = this.decline.bind(this);
+  }
+
+  accept(id, userId, organizationId) {
+    this.setState({ requestStatus: 'accepted' });
+    this.props.updateRequest({ id, userId, organizationId, status: 'accepted' })
+  }
+
+  decline(id, userId, organizationId) {
+    this.setState({ requestStatus: 'declined' });
+    this.props.updateRequest({ id, userId, organizationId, status: 'declined' })
+  }
+
+  render() {
+    const { ownUsers, organization, organizationRequests } = this.props;
+    const { accept, decline } = this;
+    return (
+      <div>
+        {
+          ownUsers.map(user => {
+            const ownRequest = organizationRequests.find(request => request.userId === user.id && request.organizationId === organization.id);
+            const { id, userId, organizationId } = ownRequest;
+            return (
+              <div key={user.id}>
+                {user.fullName}
+                <button onClick={() => accept(id, userId, organizationId)}>Accept</button>
+                <button onClick={() => decline(id, userId, organizationId)}>Decline</button>
+                { this.state.requestStatus === 'accepted' && 'Accepted' }
+                { this.state.requestStatus === 'declined' && 'Declined' }
+              </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 const mapState = ({ users, organizations, organizationRequests }, { organization }) => {
@@ -42,8 +65,7 @@ const mapState = ({ users, organizations, organizationRequests }, { organization
 
 const mapDispatch = dispatch => {
   return {
-    acceptUser: (orgReq) => dispatch(updateOrganizationRequestOnServer(orgReq)),
-    declineUser: (orgReq) => dispatch(updateOrganizationRequestOnServer(orgReq)),
+    updateRequest: (orgReq) => dispatch(updateOrganizationRequestOnServer(orgReq)),
   }
 }
 
