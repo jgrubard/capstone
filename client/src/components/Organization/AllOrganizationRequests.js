ee287f89b6bb5239c7d9373043c2cf61351db1ea
Jeremy Grubard <jgrubard@gmail.com>
2018-06-01 09:59:40 -0400

    commit

--- client/src/components/Organization/AllOrganizationRequests.js
+++ client/src/components/Organization/AllOrganizationRequests.js
@@ -5,13 +5,13 @@ import { deleteOrganizationRequestFromServer } from '../../store';
 const AllOrganizationRequests = ({ users, organizations, organizationRequests, deleteOrganizationRequest }) => {
   return (
     <div>
+      <h2>All Organization Requests</h2>
       {
         organizationRequests.map(request => {
           const user = users.find(user => user.id === request.userId)
           const organization = organizations.find(organization => organization.id === request.organizationId)
           return (
             <div key={request.id}>
-              <h2>All Organization Requests</h2>
               {organization.name} requested by {user.fullName} ({ request.status })
               <button onClick={() => deleteOrganizationRequest(request.id)}>delete</button>
             </div>
